d8ec90621c5d9a109ccabaa1219144137d84a8e0
Michał Ławinski <michrarr@gmail.com>
2023-02-15 21:00:07 +0100

    Dodanie strony do aplikacji

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+
+
+@NgModule({
+  imports: [],
+  exports: [AboutmeComponent],
+  declarations: [AboutmeComponent],
+  providers: [],
+})
+export class PagesModule { }
