f835000a44e78a00f7892066dd7d0c7bdb4bea06
Michał Ławinski <michrarr@gmail.com>
2023-02-18 23:16:09 +0100

    Zmiana wyglądu navbaru

--- src/app/layouts/layouts.module.ts
+++ src/app/layouts/layouts.module.ts
@@ -1,13 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
 import { ButtonModule } from '@syncfusion/ej2-angular-buttons';
 import { NgModule } from '@angular/core';
 import { MainComponent } from './main/main.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterCompoment } from './footer/footer.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   imports: [
-    ButtonModule
+    ButtonModule,
+    FormsModule,
+    BrowserModule
   ],
   exports: [
     NavbarComponent,
--- src/app/layouts/navbar/navbar.component.ts
+++ src/app/layouts/navbar/navbar.component.ts
@@ -1,13 +1,75 @@
-import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Component, HostListener, OnInit } from '@angular/core';
+
+interface IMenuItems {
+  id?: number | null;
+  name?: string | null;
+  url?: string | null;
+  isActive?: boolean | null;
+  icon?: string | null;
+}
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-
 export class NavbarComponent implements OnInit {
-  constructor() { }
 
-  ngOnInit() { }
+  menuItems: IMenuItems[] = [];
+  isScreenSmall = false;
+  showMenu = true;
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.menuItems = [
+      {
+        id: 1,
+        name: 'HOME',
+        url: '',
+        isActive: true
+      },
+      {
+        id: 2,
+        name: 'ABOUT ME',
+        url: '/aboutme',
+        isActive: false
+      },
+      {
+        id: 3,
+        name: 'SKILLS',
+        url: '/skills',
+        isActive: false
+      },
+      {
+        id: 4,
+        name: 'HOBBIES',
+        url: '/hobbies',
+        isActive: false
+      },
+    ];
+    window.innerWidth < 1001 ? this.isScreenSmall = true : this.isScreenSmall = false;
+  }
+
+  onMenuClick(item: IMenuItems): void {
+    this.menuItems.forEach( (menuItem) => {
+      (item.id === menuItem.id) ? menuItem.isActive = true : menuItem.isActive = false;
+    })
+    this.router.navigateByUrl(item.url!);
+  }
+
+  onIconMenuClick(): void {
+    this.showMenu = !this.showMenu;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any): void {
+    if(event.target.innerWidth < 1001) {
+      this.isScreenSmall = true;
+    } else {
+      this.isScreenSmall = false;
+      this.showMenu = true;
+    }
+  }
 }
