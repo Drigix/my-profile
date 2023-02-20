9449332f37733e1f47b33e825d75e54ca58a4eef
Michał Ławinski <michrarr@gmail.com>
2023-02-20 23:15:26 +0100

    Dodanie strony skills

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './layouts/main/main.component';
 import { AboutmeComponent } from './pages/aboutme/aboutme.component';
+import { SkillsComponent } from './pages/skills/skills.component';
 
 const routes: Routes = [];
 
@@ -15,6 +16,10 @@ const routes: Routes = [];
       {
         path: 'aboutme',
         component: AboutmeComponent
+      },
+      {
+        path: 'skills',
+        component: SkillsComponent
       }
     ])
   ],
