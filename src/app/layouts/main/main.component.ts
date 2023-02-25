8bc204866f6480b843b91638b21b381f283f26e4
Michał Ławinski <michrarr@gmail.com>
2023-02-25 22:46:23 +0100

    Nowy wygląd strony głównej

--- src/app/layouts/main/main.component.ts
+++ src/app/layouts/main/main.component.ts
@@ -1,13 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 
+interface IFrameElement {
+  id?: number | null;
+  img?: string | null;
+  style?: string | null;
+}
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-
 export class MainComponent implements OnInit {
+
+  frameElements: IFrameElement[] = [];
   constructor() { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.frameElements = [
+      {
+        id: 1,
+        img: "../../../assets/photos/politechnika-slaska__1_-removebg-preview.png",
+        style: 'height: 200px'
+      },
+      {
+        id: 2,
+        img: "../../../assets/photos/ente-removebg-preview.png",
+        style: 'height: 70px'
+      },
+      {
+        id: 3,
+        img: "../../../assets/photos/angular.png",
+        style: 'height: 140px'
+      },
+      {
+        id: 4,
+        img: "../../../assets/photos/java.png",
+        style: 'height: 100px; padding-bottom:20px;'
+      }
+    ]
+  }
 }
